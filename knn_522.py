080ab9f7a151f56e8c66b8a20d89532af75b6466
Wengran Wang <wwang33@Wengrans-MacBook-Pro.local>
2019-10-19 17:22:58 -0400

    changed knn fold from 3 to 5

--- knn_522.py
+++ knn_522.py
@@ -1,6 +1,6 @@
 from sklearn.neighbors import KNeighborsClassifier
 
 def get_model():
-    neigh = KNeighborsClassifier(n_neighbors=3)
+    neigh = KNeighborsClassifier(n_neighbors=5)
     # model = logisticRegr.fit()
     return neigh
