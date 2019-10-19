df232b960eecf2609386196cdbbef802c256a945
Wengran Wang <wwang33@Wengrans-MacBook-Pro.local>
2019-10-19 16:58:10 -0400

    cross_validation workflow finished

--- cross_validation.py
+++ cross_validation.py
@@ -1,12 +1,14 @@
 import pandas as pd 
 import logistic_regression_522 
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 from sklearn import metrics
 from sklearn.metrics import fbeta_score, make_scorer
-from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import cross_val_predict
 from sklearn_pandas import DataFrameMapper
+from sklearn.metrics import confusion_matrix
 
 
 
@@ -20,18 +22,16 @@ def make_predictions(x, y):
 
 def evaluate_predictions(x, y):
     y_pred = make_predictions(x, y)
-    def tn(y, y_pred): return confusion_matrix(y_true, y_pred)[0, 0]
-    def fp(y, y_pred): return confusion_matrix(y_true, y_pred)[0, 1]
-    def fn(y, y_pred): return confusion_matrix(y_true, y_pred)[1, 0]
-    def tp(y, y_pred): return confusion_matrix(y_true, y_pred)[1, 1]
-    tp, tn, fp, fn = make_scorer(tp),make_scorer(tn), make_scorer(fp), make_scorer(fn)
-    scoring = {'tp': make_scorer(tp), 'tn': make_scorer(tn),
-               'fp': make_scorer(fp), 'fn': make_scorer(fn)}
-    accuracy = tp + tn,
-    precision = tp / (tp + fp),
+    print(confusion_matrix(y, y_pred))
+    def tn(y, y_pred): return confusion_matrix(y, y_pred)[0, 0]
+    def fp(y, y_pred): return confusion_matrix(y, y_pred)[0, 1]
+    def fn(y, y_pred): return confusion_matrix(y, y_pred)[1, 0]
+    def tp(y, y_pred): return confusion_matrix(y, y_pred)[1, 1]
+    tp, tn, fp, fn = int(tn(y, y_pred)), int(tp(y, y_pred)), int(fn(y, y_pred)),int(fp(y, y_pred))
+    accuracy = (tp + tn)/(tp+ tn+ fp+ fn)
+    precision = tp / (tp + fp)
     recall = tp / (tp + fn)
-    f1 = 2 * precision * recall / (precision + recall)
-    print(scoring)
+    f1 = 2 * (precision) * (recall) / (precision + recall)
     print("accuracy: ", accuracy)
     print("precision: ", precision)
     print("recall: ", recall)    
@@ -43,10 +43,13 @@ def run_me():
     x = data[[ 'o_fgm', 'o_fga', 'o_ftm', 'o_fta', 'o_oreb',
        'o_dreb', 'o_reb', 'o_asts', 'o_pf', 'o_stl', 'o_to', 'o_blk', 'o_pts', 'd_fgm', 'd_fga', 'd_ftm', 'd_fta', 'd_oreb',
        'd_dreb', 'd_reb', 'd_asts', 'd_pf', 'd_stl', 'd_to', 'd_blk',  'd_pts', 'pace']]
-    y = data.class
-    mapper = DataFrameMapper([(df.columns, StandardScaler())])
-    scaled_features = mapper.fit_transform(df.copy(), 4)
-    scaled_features_df = pd.DataFrame(scaled_features, index=df.index, columns=df.columns)
+    y = data[['class']]
+    mapper = DataFrameMapper([(x.columns, StandardScaler())])
+    x = mapper.fit_transform(x, 4)
+    mapper = DataFrameMapper([(y.columns, LabelEncoder())])
+    y = mapper.fit_transform(y, 4)
+
+    # x = pd.DataFrame(x, index=x.index, columns=x.columns)
 
     evaluate_predictions(x,y)
 
